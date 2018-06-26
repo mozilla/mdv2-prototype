1561ed2fb79849081601e7fd9014f25ec2632e07
Erin Comerford <erin.comerford3@gmail.com>
2018-06-25 18:24:20 -0700

    Update view descriptions.

--- src/Components/ComparisonView.js
+++ src/Components/ComparisonView.js
@@ -1,12 +1,18 @@
 import React, {Component} from "react";
-import {Grid, Row, Tooltip} from "react-bootstrap";
+import {Grid, Row, Col} from "react-bootstrap";
 
 export class ComparisonView extends Component {
   render() {
     return (
       <Grid  className="compare-view" fluid>
         <Row>
-          <i className="fas fa-info-circle"></i> The comparison view displays the distribution of a measure across two versions.
+          <Col>
+            <i className="fas fa-info-circle"></i> The comparison view displays the distribution of a measure across two versions.
+          </Col>
+          <Col>
+            <div>Select a version below to compare against.</div>
+            <div>Hover over a point on the graph to view a specific set of values.</div>
+          </Col>
         </Row>
       </Grid>
     )
--- src/Components/EvolutionView.js
+++ src/Components/EvolutionView.js
@@ -1,14 +1,18 @@
 import React, {Component} from "react";
-import {Grid, Row} from "react-bootstrap";
+import {Grid, Row, Col} from "react-bootstrap";
 
 export class EvolutionView extends Component {
   render() {
     return (
       <Grid  className="evolve-view" fluid>
         <Row>
-          <p>
+          <Col>
             <i className="fas fa-info-circle"></i> The evolution view shows the evolution of a given aggregate over time.
-          </p>
+          </Col>
+          <Col>
+            <div>Select an aggregate type to compare, and choose a version below to compare against.</div>
+            <div>Hover over a point on the graph to view a specific set of values.</div>
+          </Col>
         </Row>
       </Grid>
     )
