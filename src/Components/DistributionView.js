cecb18ff8bded9dcaaffb2dedabcc3764e087b53
Erin Comerford <erin.comerford3@gmail.com>
2018-06-29 10:25:36 -0700

    Remove unnecessary white space, correct indents.

--- src/Components/DistributionView.js
+++ src/Components/DistributionView.js
@@ -20,7 +20,7 @@ export class DistributionView extends Component {
     return (
       <Grid  className="dist-view" fluid>
         <Row>
-          <Col >
+          <Col>
             <i className="fas fa-info-circle"></i> The distribution view displays the distribution of user outcomes as a histogram.
             <div>Hover over a point on the graph to view a specific value.</div>
             <div>Or, switch to table mode below to see a list of all absolute values.</div>
@@ -38,19 +38,19 @@ export class DistributionView extends Component {
           </ToggleButtonGroup>
         </Row>
         <Row>
-        {this.state.mode === "graph" &&
-          <MetricsGraphics
-            title={this.props.activeMetric}
-            data={this.props.currentData}
-            x_label={this.props.activeMetric}
-            y_label="Proportion of Users"
-            chart_type="histogram"
-            width={600}
-            height={250}
-            show_rollover_text={true}
-            y_accessor="proportion"
-            x_accessor="start"
-          />
+          {this.state.mode === "graph" &&
+            <MetricsGraphics
+              title={this.props.activeMetric}
+              data={this.props.currentData}
+              x_label={this.props.activeMetric}
+              y_label="Proportion of Users"
+              chart_type="histogram"
+              width={600}
+              height={250}
+              show_rollover_text={true}
+              y_accessor="proportion"
+              x_accessor="start"
+            />
           }
           {this.state.mode === "table" &&
             <TableMode
