a9d2791a78582ba069e39efa458b15884ae0d973
Erin Comerford <erin.comerford3@gmail.com>
2018-07-05 21:01:59 -0700

    Add get percentile function.

--- src/App.js
+++ src/App.js
@@ -6,47 +6,17 @@ import {MetricSelector} from "./Components/MetricSelector.js";
 import {VersionSelector} from "./Components/VersionSelector.js";
 import {ChannelSelector} from "./Components/ChannelSelector.js";
 import metricData from "./data/metrics.js";
+import GC_MS_nightly_62 from "./data/GC_MS_nightly_62.json"
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentData: [
-        {
-          "start": 0,
-          "end": 1,
-          "count": 1623,
-          "proportion": 0.000011537046015254444
-        },
-        {
-          "start": 1,
-          "end": 2,
-          "count": 41,
-          "proportion": 2.8913714335682874e-7
-        },
-        {
-          "start": 2,
-          "end": 3,
-          "count": 201,
-          "proportion": 0.0000014314658572747914
-        },
-        {
-          "start": 3,
-          "end": 4,
-          "count": 878,
-          "proportion": 0.000006240148348020738
-        },
-        {
-          "start": 4,
-          "end": 5,
-          "count": 2101,
-          "proportion": 0.00001493796241457452
-        },
-      ],
+      currentData: GC_MS_nightly_62,
       change: "",
-      nfifthPercentile: 99,
-      median: 42,
-      lastMedian: 40,
+      nfifthPercentile: "",
+      median: "",
+      lastMedian: 400,
       activeMetric: "GC_MS",
       metricOptions: metricData,
       activeVersion: "62",
@@ -56,59 +26,60 @@ class App extends React.Component {
     }
   }
 
-<<<<<<< Updated upstream
   componentWillMount = () => {
-    fetch("https://mozilla.github.io/mdv2/public/data" + this.state.activeMetric + "_" + this.state.activeChannel + "_" + this.state.activeVersion + ".json")
-      .then(response => response.json())
-      .then(data => this.setState({
-        currentData: data,
-      }));
-  }
-
-=======
->>>>>>> Stashed changes
-  componentDidMount = () => {
-    var med = this.getPercentile(50);
-    var nfifth = this.getPercentile(95);
-    this.getChange();
+    var med = this.getPercentile(50).toFixed(2);
+    var nfifth = this.getPercentile(95).toFixed(2);
     this.setState({
       median: med,
       nfifthPercentile: nfifth,
     });
   }
 
+  componentDidMount = () => {
+    this.getChange();
+  }
+
   getLastBucketUpper = () => {
-    if (this.state.currentData.length === 1) return this.state.currentData[0] + 1;
-    return this.state.currentData[this.state.currentData.length - 1] + this.state.currentData[this.state.currentData.length - 1]
-        - this.state.currentData[this.state.currentData.length -2];
+    var buckets = this.state.currentData.map(item => item.start);
+    var lastBucketUpper;
+    console.log(buckets);
+    if (this.state.currentData.length === 1) {
+      lastBucketUpper = buckets[0] + 1;
+    } else {
+      /*if (this.state.activeMetric.type === "linear" || this.state.activeMetric.type === "flag" || this.state.activeMetric.type ===
+      "boolean" || this.state.activeMetric.type === "enumerated") {
+        lastBucketUpper = buckets[buckets.length - 1] + buckets[buckets.length - 1]
+            - buckets[buckets.length -2];
+      } else {*/
+      lastBucketUpper = buckets[buckets.length - 1] *
+      buckets[buckets.length - 1] /
+      buckets[buckets.length - 2];
+      //}
+    }
+    return lastBucketUpper;
   };
 
-
   getPercentile = (percentile) => {
-    var starts = this.state.currentData.map(item =>
-      [item.start]);
-    var buckets = starts.concat([this.getLastBucketUpper()]);
-    // find the distance between last two buckets.
-    var linearTerm = buckets[buckets.length - 1] - buckets[buckets.length - 2];
-    // Adds up total count and multiplies by percentile passed to find the # of counts that lie below the percentile line.
-    var percentileCount = this.state.currentData.reduce(function (previous, count) {
-      return previous + count;
-    }, 0) * (percentile / 100);
-
+    var buckets = this.state.currentData.map(item => item.start);
+    buckets = buckets.concat([this.getLastBucketUpper()]);
+    var values = this.state.currentData.map(item => item.count);
+    //var linearTerm = buckets[buckets.length - 1] - buckets[buckets.length - 2];
+    var exponentialFactor = buckets[buckets.length - 1] / buckets[buckets.length - 2];
+    var percentileCount = values.reduce((accumulator, currentValue) => accumulator + currentValue, 0) * (percentile / 100);
     var percentileBucketIndex = 0;
-
-    // Increment index from 0 while subtracting bucket count from PercentileInBar for each bucket/index until PercentileInBar is less than zero, then subtract 1 to index. That is the index of the bucket that holds the percentile.
     while (percentileCount >= 0) {
-      percentileCount -= this.state.currentData[percentileBucketIndex];
+      percentileCount -= this.state.currentData[percentileBucketIndex].count;
       percentileBucketIndex++;
     }
     percentileBucketIndex--;
-    // Add back the last bucket count.
-    percentileCount += this.state.currentData[percentileBucketIndex];
-    // Divide PercentileInBar by count in index bucket to get ratio of the hits in the percentile to the hits in the bar containing it
-    var ratioInBar = percentileCount / this.state.currentData[percentileBucketIndex];
-    console.log(starts, buckets, linearTerm, percentileCount);
-    return buckets[percentileBucketIndex] + linearTerm * ratioInBar;
+    percentileCount += this.state.currentData[percentileBucketIndex].count;
+    var ratioInBar = percentileCount / this.state.currentData[percentileBucketIndex].count;
+    /*if (this.kind === "linear" || this.kind === "flag" || this.kind ===
+    "boolean" || this.kind === "enumerated") {
+      return buckets[percentileBucketIndex] + linearTerm * ratioInBar;
+    } else {*/
+    return buckets[percentileBucketIndex] * Math.pow(exponentialFactor, ratioInBar);
+    //}
   };
 
   onMetricChange = (value) => {
