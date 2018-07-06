fd986d320e1397b962bc985da5686531a66d7bd4
Erin Comerford <erin.comerford3@gmail.com>
2018-07-05 20:07:21 -0700

    Move data to public.

--- src/App.js
+++ src/App.js
@@ -56,61 +56,6 @@ class App extends React.Component {
     }
   }
 
-<<<<<<< Updated upstream
-  componentWillMount = () => {
-    fetch("https://mozilla.github.io/mdv2/public/data" + this.state.activeMetric + "_" + this.state.activeChannel + "_" + this.state.activeVersion + ".json")
-      .then(response => response.json())
-      .then(data => this.setState({
-        currentData: data,
-      }));
-  }
-
-=======
->>>>>>> Stashed changes
-  componentDidMount = () => {
-    var med = this.getPercentile(50);
-    var nfifth = this.getPercentile(95);
-    this.getChange();
-    this.setState({
-      median: med,
-      nfifthPercentile: nfifth,
-    });
-  }
-
-  getLastBucketUpper = () => {
-    if (this.state.currentData.length === 1) return this.state.currentData[0] + 1;
-    return this.state.currentData[this.state.currentData.length - 1] + this.state.currentData[this.state.currentData.length - 1]
-        - this.state.currentData[this.state.currentData.length -2];
-  };
-
-
-  getPercentile = (percentile) => {
-    var starts = this.state.currentData.map(item =>
-      [item.start]);
-    var buckets = starts.concat([this.getLastBucketUpper()]);
-    // find the distance between last two buckets.
-    var linearTerm = buckets[buckets.length - 1] - buckets[buckets.length - 2];
-    // Adds up total count and multiplies by percentile passed to find the # of counts that lie below the percentile line.
-    var percentileCount = this.state.currentData.reduce(function (previous, count) {
-      return previous + count;
-    }, 0) * (percentile / 100);
-
-    var percentileBucketIndex = 0;
-
-    // Increment index from 0 while subtracting bucket count from PercentileInBar for each bucket/index until PercentileInBar is less than zero, then subtract 1 to index. That is the index of the bucket that holds the percentile.
-    while (percentileCount >= 0) {
-      percentileCount -= this.state.currentData[percentileBucketIndex];
-      percentileBucketIndex++;
-    }
-    percentileBucketIndex--;
-    // Add back the last bucket count.
-    percentileCount += this.state.currentData[percentileBucketIndex];
-    // Divide PercentileInBar by count in index bucket to get ratio of the hits in the percentile to the hits in the bar containing it
-    var ratioInBar = percentileCount / this.state.currentData[percentileBucketIndex];
-    console.log(starts, buckets, linearTerm, percentileCount);
-    return buckets[percentileBucketIndex] + linearTerm * ratioInBar;
-  };
-
   onMetricChange = (value) => {
     this.setState({
       activeMetric: value,
