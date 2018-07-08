6f8de035b5b3a39483f3076a19f6f0408343d654
Erin Comerford <erin.comerford3@gmail.com>
2018-07-08 13:52:37 -0700

    edit fetch-mock import.

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import fetchMock from "fetch-mock";
+import fetchMock from "./../node_modules/fetch-mock";
 
 fetchMock.get("*", {hello: "world"});
 
