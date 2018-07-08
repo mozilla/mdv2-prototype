8f659a271aefd07fa960ba8718297035e83e0db7
Erin Comerford <erin.comerford3@gmail.com>
2018-07-08 13:30:27 -0700

    Add fetch-mock to test file.

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App, getCurrentData from './App';
+import fetchMock from "fetch-mock";
+
+fetchMock.get("https://mozilla.github.io/mdv2/data/GC_MS_nightly_62.json", {hello: "world"});
+
+getCurrentData().then(function(data) {
+  console.log("got data", data);
+});
+
+fetchMock.restore();
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
