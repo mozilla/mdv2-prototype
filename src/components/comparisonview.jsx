6f4922124cebfe1437bf073a06935af640c10d07
Erin Comerford <erin.comerford3@gmail.com>
2018-08-29 10:19:53 -0700

    Add plotly bar overlay chart. (#49)
    * Add plotly bar overlay chart.
    
    * Use window resize listeners to measure the width available so we can use it to size the plot's svg
    
    * Use plotly.js to plot Distributions
    
    * Make plotly testable
    
    * Rebase to moz master, update plotly graph titles.

--- src/components/comparisonview.jsx
+++ src/components/comparisonview.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import {Grid, Row, Col, DropdownButton, MenuItem} from "react-bootstrap";
 import MetricsGraphics from "react-metrics-graphics";
+import Plot from "react-plotly.js";
 import GC_MS_nightly_61 from "./../data/GC_MS_nightly_61";
 
 export class ComparisonView extends Component {
@@ -20,6 +21,9 @@ export class ComparisonView extends Component {
       this.props.dataStore.active.data,
       GC_MS_nightly_61
     ];
+    let x1 = GC_MS_nightly_61.map(e => e.start);
+    let y1 = GC_MS_nightly_61.map(e => e.count);
+    let y2 = this.props.dataStore.active.data.map(e => e.count);
 
     return (
       <Grid  className="comparison view" fluid>
@@ -65,6 +69,41 @@ export class ComparisonView extends Component {
               y_accessor="proportion"
               x_accessor="start"
               area={[true, true]}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Plot
+              data={[
+                {
+                  x: x1,
+                  y: y1,
+                  type: "bar",
+                  name: this.props.dataStore.active.channel + " " + this.state.compareVersion,
+                  opacity: 0.5,
+                  mode: "markers",
+                },
+                {
+                  x: x1,
+                  y: y2,
+                  type: "bar",
+                  name: this.props.dataStore.active.channel + " " + this.props.dataStore.active.version,
+                  opacity: 0.6,
+                  mode: "markers",
+                },
+              ]}
+              layout={{
+                barmode: "overlay",
+                title: this.props.dataStore.active.metric,
+                xaxis: {
+                  type: "category",
+                  title: this.props.dataStore.active.metric,
+                },
+                yaxis: {
+                  title: "Number of Users",
+                },
+              }}
             />
           </Col>
         </Row>
