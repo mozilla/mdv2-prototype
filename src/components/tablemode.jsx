0a8089afea2728647a8726bc950fb2742030125d
Chris H-C <chutten@mozilla.com>
2018-09-04 15:59:17 -0400

    Augment tablemode to support categorical data

--- src/components/tablemode.jsx
+++ src/components/tablemode.jsx
@@ -2,7 +2,33 @@ import React, {Component} from "react";
 import {Table} from "react-bootstrap";
 
 export class TableMode extends Component {
-  render() {
+  renderLabel() {
+    return (
+      <div className="table-mode">
+        <h3>{this.props.activeMetric}</h3>
+        <Table striped bordered condensed>
+          <thead>
+            <tr>
+              <th>Label</th>
+              <th>Count</th>
+              <th>Proportion</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.currentData.map( (data, index) =>
+              <tr key={index}>
+                <td>{data.label}</td>
+                <td>{data.count}</td>
+                <td>{data.proportion}</td>
+              </tr>
+            )}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
+
+  renderStartEnd() {
     return (
       <div className="table-mode">
         <h3>{this.props.activeMetric}</h3>
@@ -28,5 +54,13 @@ export class TableMode extends Component {
         </Table>
       </div>
     );
+  }
+
+  render() {
+    if (this.props.currentData[0] && this.props.currentData[0].label) {
+      return this.renderLabel();
+    } else {
+      return this.renderStartEnd();
+    }
   }
 }
