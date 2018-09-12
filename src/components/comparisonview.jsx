f483b59852ae00126afa3c6ecde6afa535346457
Chris H-C <chutten@mozilla.com>
2018-09-12 16:17:38 -0400

    Comparison View: Allow comparing to things other than GC_MS nightly 61.

--- src/components/comparisonview.jsx
+++ src/components/comparisonview.jsx
@@ -1,13 +1,16 @@
 import React, {Component} from "react";
 import {Grid, Row, Col, DropdownButton, MenuItem} from "react-bootstrap";
 import Plot from "react-plotly.js";
-import GC_MS_nightly_61 from "./../data/GC_MS_nightly_61";
+import {MetricData} from "../metricdata.js";
 
 export class ComparisonView extends Component {
   constructor(props) {
     super(props);
+    this.dataStore = new MetricData();
+    this.prevProps = {};
     this.state = {
       compareVersion: "61",
+      compareData: this.dataStore.active
     };
   }
 
@@ -19,6 +22,11 @@ export class ComparisonView extends Component {
     this.setState({compareVersion: event});
   }
 
+  async loadDataToState(metric, channel, version) {
+    await this.dataStore.loadDataFor(metric, channel, version);
+    this.setState({compareData: this.dataStore.active});
+  }
+
   componentDidMount() {
     window.addEventListener("resize", this.onResize);
     this.onResize();
@@ -28,20 +36,39 @@ export class ComparisonView extends Component {
     window.removeEventListener("resize", this.onResize);
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    const {metric, channel, version} = this.props.dataStore.active;
+    if (this.prevProps.metric !== metric ||
+      this.prevProps.channel !== channel ||
+      this.prevProps.version !== version ||
+      prevState.compareVersion !== this.state.compareVersion
+    ) {
+      this.loadDataToState(this.props.dataStore.active.metric, this.props.dataStore.active.channel, this.state.compareVersion);
+    }
+
+    this.prevProps = {
+      metric: prevProps.dataStore.active.metric,
+      channel: prevProps.dataStore.active.channel,
+      version: prevProps.dataStore.active.version
+    };
+  }
+
   render() {
-    if (this.props.dataStore.active.metric !== "GC_MS") {
+    const {metric, channel, version} = this.props.dataStore.active;
+    const COMPARABLE_MEASURES = [
+      "GC_MS",
+      "scalars_timestamps_first_paint",
+      "scalars_browser_engagement_tab_open_event_count",
+    ];
+    if (!COMPARABLE_MEASURES.includes(metric) || !this.state.compareData) {
       return (
-        <div>Cannot compare {this.props.dataStore.active.metric} to GC_MS</div>
+        <div>Comparison View is currenty not supported for <span className="metric-name">{this.props.dataStore.active.metric}</span></div>
       );
     }
 
-    let x1 = GC_MS_nightly_61.map(e => e.start);
-    let y1 = GC_MS_nightly_61.map(e => e.count);
+    let x1 = this.state.compareData.data.map(e => e.start);
+    let y1 = this.state.compareData.data.map(e => e.count);
     let y2 = this.props.dataStore.active.data.map(e => e.count);
-
-    const metricName = this.props.dataStore.active.metric;
-    const activeChannel = this.props.dataStore.active.channel;
-    const activeVersion = this.props.dataStore.active.version;
 
     return (
       <Grid  className="comparison view" fluid>
@@ -84,7 +111,7 @@ export class ComparisonView extends Component {
                   x: x1,
                   y: y1,
                   type: "bar",
-                  name: activeChannel + " " + this.state.compareVersion,
+                  name: channel + " " + this.state.compareVersion,
                   opacity: 0.5,
                   mode: "markers",
                 },
@@ -92,17 +119,17 @@ export class ComparisonView extends Component {
                   x: x1,
                   y: y2,
                   type: "bar",
-                  name: activeChannel + " " + activeVersion,
+                  name: channel + " " + version,
                   opacity: 0.6,
                   mode: "markers",
                 },
               ]}
               layout={{
                 barmode: "group",
-                title: metricName,
+                title: metric,
                 xaxis: {
                   type: "category",
-                  title: metricName,
+                  title: metric,
                 },
                 yaxis: {
                   title: "Number of Users",
